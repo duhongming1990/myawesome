278a3693e539b24bdb9cce8e162689a07947b5fe
duhongming <duhongming@lixiang.com>
2023-11-08 13:14:14 +0800

    init some project

--- submodule-add.sh
+++ submodule-add.sh
@@ -1,5 +1,9 @@
 git submodule add git@github.com:duhongming1990/awesome.git
 git submodule add git@github.com:duhongming1990/awesome-java.git
 git submodule add git@github.com:duhongming1990/awesome-selfhosted.git
+git submodule add git@github.com:duhongming1990/awesome-python.git
+git submodule add git@github.com:duhongming1990/awesome-micropython.git
+git submodule add git@github.com:duhongming1990/awesome-go.git
+
 
 
