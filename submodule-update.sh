27e0798887745027cbf2b724a65fd376154d5b87
duhongming <duhongming@lixiang.com>
2023-11-08 15:09:30 +0800

    add submodule-update.sh

---
+++ submodule-update.sh
@@ -0,0 +1,6 @@
+git submodule update --init awesome-goawesome
+git submodule update --init awesome-java
+git submodule update --init awesome-selfhosted
+git submodule update --init awesome-python
+git submodule update --init awesome-micropython
+git submodule update --init awesome-go
