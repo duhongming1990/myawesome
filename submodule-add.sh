f86615a089b9c3f2b6e144005883d062c9df239a
duhongming <duhongming@lixiang.com>
2023-11-08 11:40:03 +0800

    add awesome md

---
+++ submodule-add.sh
@@ -0,0 +1,5 @@
+git submodule add git@github.com:duhongming1990/awesome.git
+git submodule add git@github.com:duhongming1990/awesome-java.git
+git submodule add git@github.com:duhongming1990/awesome-selfhosted.git
+
+
